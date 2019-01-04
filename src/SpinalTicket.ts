d5c22690842240c2c1ac86ea01b3f46b6824c8d2
LorisC <loris@ciprin.fr>
2019-01-04 15:58:25 +0100

    [UPDATE]

--- src/SpinalTicket.ts
+++ src/SpinalTicket.ts
@@ -1,4 +1,29 @@
-import {Model} from "spinal-core-connectorjs_type"
+/*
+ * Copyright 2019 SpinalCom - www.spinalcom.com
+ *
+ *  This file is part of SpinalCore.
+ *
+ *  Please read all of the following terms and conditions
+ *  of the Free Software license Agreement ("Agreement")
+ *  carefully.
+ *
+ *  This Agreement is a legally binding contract between
+ *  the Licensee (as defined below) and SpinalCom that
+ *  sets forth the terms and conditions that govern your
+ *  use of the Program. By installing and/or using the
+ *  Program, you agree to abide by all the terms and
+ *  conditions stated or referenced herein.
+ *
+ *  If you do not agree to abide by these terms and
+ *  conditions, do not demonstrate your acceptance and do
+ *  not install or use the Program.
+ *  You should have received a copy of the license along
+ *  with this file. If not, see
+ *  <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
+import { Model } from "spinal-core-connectorjs_type"
+
 const spinalCore = require("spinal-core-connectorjs");
 
 export interface TicketInterface {
@@ -9,6 +34,8 @@ export interface TicketInterface {
     equipment?: string;
     state?: string;
     creationDate?: number;
+  //id of the real problem
+  sourceId?: string;
     images?: string[];
     urls?: string[];
     categories?: string[];
