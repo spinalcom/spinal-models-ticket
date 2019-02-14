e6f90b9983ceadf637574751534a6bcde11837ef
LorisC <loris@ciprin.fr>
2019-02-14 16:54:56 +0100

    add log

--- declarations/SpinalLogTicket.d.ts
+++ declarations/SpinalLogTicket.d.ts
@@ -1,7 +1,11 @@
-export interface SpinalLogTicket {
+import { Model } from "spinal-core-connectorjs_type";
+export interface SpinalLogTicketInterface {
     id?: string;
     ticketId?: string;
     date?: number;
     steps: string[];
     [key: string]: any;
+}
+export default class SpinalLogTicket extends Model {
+    constructor(log: SpinalLogTicketInterface);
 }
